065d6f9a19dc9bc14c9cfd059f0c9983a3c86a1e
cvdlbot <tony.joseph@uoit.net>
2019-02-13 19:10:08 -0500

    added new attention cnn network

--- src/nets.py
+++ src/nets.py
@@ -44,6 +44,45 @@ def net(images, mode='train'):
 
         return layer_4
 
+def attn_cnn(images, mode='train'):
+    with tf.variable_scope('Attention_CNN'):
+        layer_1 = slim.conv2d(images, 128, [5, 5],
+                            activation_fn=None,
+                            padding='SAME',
+                            weights_initializer=tf.contrib.layers.variance_scaling_initializer(mode='FAN_IN'),
+                            stride=2, scope='layer_1')
+        layer_1 = _batch_norm(layer_1, mode=mode, name='layer_1')
+        layer_1 = tf.nn.leaky_relu(layer_1, name='relu_layer_1')
+        print(layer_1.get_shape())
+
+        layer_2 = slim.conv2d(layer_1, 128, [3, 3],
+                            activation_fn=None,
+                            padding='SAME',
+                            weights_initializer=tf.contrib.layers.variance_scaling_initializer(mode='FAN_IN'),
+                            stride=2, scope='layer_2')
+        layer_2 = _batch_norm(layer_2, mode=mode, name='layer_2')
+        layer_2 = tf.nn.leaky_relu(layer_2, name='relu_layer_2')
+        print(layer_2.get_shape())
+
+        layer_3 = slim.conv2d(layer_2, 128, [3, 3],
+                            activation_fn=None,
+                            padding='SAME',
+                            weights_initializer=tf.contrib.layers.variance_scaling_initializer(mode='FAN_IN'),
+                            stride=1, scope='layer_3')
+        layer_3 = _batch_norm(layer_3, mode=mode, name='layer_3')
+        layer_3 = tf.nn.leaky_relu(layer_3, name='relu_layer_3')
+        print(layer_3.get_shape())
+
+        layer_4 = slim.conv2d(layer_3, 128, [3, 3],
+                            activation_fn=None,
+                            padding='VALID',
+                            weights_initializer=tf.contrib.layers.variance_scaling_initializer(mode='FAN_IN'),
+                            stride=2, scope='layer_4')
+        layer_4 = _batch_norm(layer_4, mode=mode, name='layer_4')
+        print(layer_4.get_shape())
+
+        return layer_4
+
 def classification_network(images, dropout, mode='train'):
     with tf.variable_scope('classification_CNN'):
         layer_1 = slim.conv2d(images, 64, [5, 5],
