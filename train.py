a7d20b9887e2c2d3d92e20d36d68b843c575e90f
Mohmmed Baeenh <mbaeenh@gpu1.amirali.local>
2019-02-25 12:35:49 -0500

    changed train data passing

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ class Train(object):
                 curr_loss = 0
                 start_t   = time.time()
                 for i in range(n_iters_per_epoch):
-                    image_batch, grd_bboxes_batch, grd_attn_batch, _ = next(train_loader)
+                    _, image_batch, grd_bboxes_batch, grd_attn_batch = next(train_loader)
                     feed_dict = {self.model.images: image_batch,
                                  self.model.bboxes: grd_bboxes_batch,
                                  self.model.gnd_attn: grd_attn_batch,
@@ -128,7 +128,7 @@ def main():
     # Load Trainer
     trainer = Train(model, data, val_data=None, n_epochs=1000, batch_size=64,
                     update_rule='adam', learning_rate=0.0001, print_every=100, save_every=5,
-                    pretrained_model=None, model_path='model/lstm2/', log_path='log2/')
+                    pretrained_model=None, model_path='model/lstm1/', log_path='log1/')
     # Begin Training
     trainer.train()
 #-------------------------------------------------------------------------------
