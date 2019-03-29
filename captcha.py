7ebcd48348a2d16ad48e72e8322c902210118ed6
tony joseph <tony.joseph@uoit.net>
2019-03-29 11:42:04 -0400

    updated

--- captcha.py
+++ captcha.py
@@ -60,19 +60,6 @@ class ImageCaptcha(object):
         ])
         return self._truefonts
 
-    @staticmethod
-    def create_noise_curve(image, color):
-        w, h = image.size
-        x1 = random.randint(0, int(w / 5))
-        x2 = random.randint(w - int(w / 5), w)
-        y1 = random.randint(int(h / 5), h - int(h / 5))
-        y2 = random.randint(y1, h - int(h / 5))
-        points = [x1, y1, x2, y2]
-        end = random.randint(160, 200)
-        start = random.randint(0, 20)
-        Draw(image).arc(points, start, end, fill=color)
-        return image
-
     def create_captcha_image(self, chars, color, background):
         """Create the CAPTCHA image itself.
         :param chars: text to be generated.
@@ -150,7 +137,6 @@ class ImageCaptcha(object):
         background = (255, 255, 255)
         color = random_color(10, 200, random.randint(220, 255))
         im = self.create_captcha_image(chars, color, background)
-        self.create_noise_curve(im, color)
         im = im.filter(ImageFilter.SMOOTH)
 
         return im
